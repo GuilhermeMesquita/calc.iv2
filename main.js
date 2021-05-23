888ba9be6731c1744c32a4ac3f74875edf049d7b
GuilhermeMesquita <guilhermemesquitarch@gmail.com>
2021-05-23 19:01:11 -0300

    feito reajustes de algumas funções

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var controle = false;
 var historico_div_text = document.getElementById("hist");
 
 // Eventos para o clique de cada botão
-function aposCarregar() {
+const aposCarregar = () => {
     $("#btnAC").on("click", function () {
         limpaHistorico();
     });
@@ -28,7 +28,7 @@ function aposCarregar() {
 }
 
 // Função responsável por atualizar o input principal da calculadora.
-function atualizaDisplay() {
+const atualizaDisplay = () => {
     $(".input-field").text(valor_input);
     if ($(".input-field").text().length > 8) {
         alert("Máximo de caracteres excedido.");
@@ -38,7 +38,7 @@ function atualizaDisplay() {
 }
 
 // Armazena os operadores, fazendo um callback na hora de montar o histórico.
-function sinal(operador) {
+const sinal = (operador) => {
     switch (operador) {
         case '-': calculo('-');
             break;
@@ -60,13 +60,13 @@ function calculo(symbol) {
 }
 
 // Função responsável por inverter o sinal do input field.
-function trocaSinal() {
-    valor_input = (valor_input * -1);
+const trocaSinal = () => {
+    valor_input = valor_input * -1;
     atualizaDisplay();
 }
 
 // Função referente aos números da calculadora.
-function botao(elem) {
+const botao = (elem) => {
     if (numero_novo == true) {
         valor_input = '' + elem;
         numero_novo = false;
@@ -77,14 +77,14 @@ function botao(elem) {
 }
 
 // Função ref. ao botão All Clear.
-function limpaHistorico() {
+const limpaHistorico = () => {
     numero_novo = true;
     valor_input = '0';
     historico_div_text.innerText = "";
     atualizaDisplay();
 }
 // Função ref. ao botão Clear.
-function limpaUltimoInserido() {
+const limpaUltimoInserido = () => {
     let historico_completo = historico_div_text.innerText.split(" ").join("");
     let provisorio = historico_completo.substring(0, historico_completo.length - 3);
     historico_div_text.innerText = provisorio;
@@ -92,11 +92,23 @@ function limpaUltimoInserido() {
 }
 
 // Função para mostrar o resultado, recebe como parâmetro o histórico, sendo que é retornado o calculo da expressão inteira.
-function mostraRes(operacao) {
+const mostraRes = (operacao) => {
     historico_div_text.innerText += valor_input
     let full_operacao = operacao.innerText.split(" ").join("");
     let calculo_expressao = new Function('return ' + full_operacao)();
-    valor_input = calculo_expressao;
+    let sucessor_virgula = calculo_expressao.toFixed(2).split(".")[1];
+    for (var i = 0; i < sucessor_virgula.length; i++) {
+        let valor_quebrado = sucessor_virgula.split("");
+        if (valor_quebrado[0] > 5) {
+            let prov = Number(calculo_expressao.toFixed(2).split(".")[0]) + 1;
+            var res = parseInt(prov);
+        } else {
+            prov = Number(calculo_expressao.toFixed(2).split(".")[0]);
+            res = parseInt(prov);
+        }
+        valor_input = res;
+    }
+
     atualizaDisplay();
     historico_div_text.innerText = "";
     numero_novo = true;
